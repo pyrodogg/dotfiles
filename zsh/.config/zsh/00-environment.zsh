0d4d90187c57496a9d1c5c38916e8bf514729244
Skyler Kehren <pyrodogg@gmail.com>
2019-09-08 14:46:52 +0300

    Remove username

--- zsh/.config/zsh/00-environment.zsh
+++ zsh/.config/zsh/00-environment.zsh
@@ -2,7 +2,7 @@
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 # Path to your oh-my-zsh installation.
-  export ZSH=/home/skyler/.oh-my-zsh
+  export ZSH=${HOME}/.oh-my-zsh
 #  export TERM="xterm-256color"
 if [ -n "$TMUX" ]; then
   export TERM=xterm-256color
