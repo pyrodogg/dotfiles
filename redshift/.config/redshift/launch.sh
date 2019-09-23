127fc62daf916ce896a20ca99deb5e1d5619a32d
Skyler Kehren <pyrodogg@gmail.com>
2019-09-24 01:31:19 +0300

    add redshift

---
+++ redshift/.config/redshift/launch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Terminate already running redshift instances
+killall -q redshift
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x redshift >/dev/null; do sleep 1; done
+
+# Launch redshift
+redshift
+
+echo "Redshift launched..."
