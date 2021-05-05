7d182e661343d1d223ba78fcf21762dfdf5ea9bd
Skyler Kehren <pyrodogg@gmail.com>
2021-05-05 08:56:56 +0300

    Fix compton launch, replace polybar

--- compton/.config/compton/launch.sh
+++ compton/.config/compton/launch.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-killall -q polybar
+killall -q compton
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x compton >/dev/null; do sleep 1; done
 
 compton --no-faiding-openclose --config ~/.config/compton/config -b --logpath /tmp/compton
 
