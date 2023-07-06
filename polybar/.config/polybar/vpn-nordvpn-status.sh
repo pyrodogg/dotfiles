e2d775a40706b5af7babd4bc1ac3a367be6b5a6c
Skyler Kehren <pyrodogg@gmail.com>
2023-07-06 09:04:12 -0500

    Update check for known wifi networks
    Update refresh timer on vpn badge

--- polybar/.config/polybar/vpn-nordvpn-status.sh
+++ polybar/.config/polybar/vpn-nordvpn-status.sh
@@ -2,15 +2,23 @@
 # https://github.com/polybar/polybar-scripts/tree/master/polybar-scripts/vpn-nordvpn-status
 
 #CONNECTION=$(nmcli -f name,device connection show | grep wlp2s0 | cut -d ' ' -f1)
+KNOWNWIFI="$HOME/.knownwifi"
 CONNECTION=$(wicd-cli -dy | grep Essid | cut -d ' ' -f2)
 STATUS=$(nordvpn status | grep Status | tr -d ' ' | cut -d ':' -f2)
 
+
 if [ "$STATUS" = "Connected" ]; then
-    echo "%{F#82E0AA}%{A1:nordvpn d:}$(nordvpn status | grep City | cut -d ':' -f2)%{A}%{F-}"
+  echo "%{F#82E0AA}%{A1:nordvpn d:}$(nordvpn status | grep City | cut -d ':' -f2)%{A}%{F-}"
 else
-    if [[ ${CONNECTION} =~ $HOME_WIFI* ]]; then
-        echo ""
-    else
-        echo "%{F#f00}%{A1:nordvpn c:}no vpn%{A}%{F-}"
-    fi
+
+  if grep -q -x -F "${CONNECTION}" "${KNOWNWIFI}"; then  
+    # Connected to known wifi, no warning
+    echo ""
+  else
+    # TODO Add test for network connection, ex lower warning on wpa secured wifi, complain LOUDLY if unsecured
+    # Display either the vpn connection name, or 'No VPN'
+    # %{F#f00} - color formatting RED
+    # %{A1 action on left click
+    echo "%{A1:nordvpn c:}%{F#f00}no vpn%{F-}%{A}"
+  fi
 fi
